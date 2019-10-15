7e11a0e698a0e8f53c15b8900407d6c78d76f237
Andy <andy19970307@gmail.com>
2019-10-16 00:18:42 +0800

    Add

--- catkin_ws/src/pose_estimate/arm_control/src/arm_control.py
+++ catkin_ws/src/pose_estimate/arm_control/src/arm_control.py
@@ -4,23 +4,35 @@ import roslib
 import rospy
 from text_msgs.srv import *
 from text_msgs.msg import *
+from arm_operation.srv import *
+from arm_operation.msg import *
+from std_srvs.srv import Trigger, TriggerResponse, Empty
 
-mani_req = manipulationRequest()
+Joint_Server = "/ur5_control_server/ur_control/goto_joint_pose"
+Pose_Server = "/ur5_control_server/ur_control/goto_pose"
+Straight_Server = "/ur5_control_server/ur_control/go_straight"
+
+# /ur5_control_server/ur_control/get_robot_state
+# /ur5_control_server/ur_control/go_straight
+# /ur5_control_server/ur_control/goto_joint_pose
+# /ur5_control_server/ur_control/goto_pose
+# /ur5_control_server/ur_control/stop_program
+# /ur5_control_server/ur_control/unlock_protective
 
 class arm_control(object):
     def __init__(self):
-        self.arm_move = rospy.Service("", , self.switch_callback)
+        self.mani_req = manipulationRequest()
 
-    def srv_callback(self, userdata):
-        global mani_req
-        mani_req =  manipulationRequest()
+        self.arm_move_srv = rospy.Service("~/move_to", Trigger, self.srv_callback)
+        self.arm_home_srv = rospy.Service("~/home", Trigger, self.srv_home)
 
-        per_res = self.per_client() #Call service
-        pose_req = pose_stateRequest()
-        pose_req.image = per_res.image
-        pose_req.depth = per_res.depth 
-        pose_req.mask = per_res.mask
-        pose_res = self.pose_client(pose_req)
+    def srv_callback(self, req):
+
+        print pose_res.count 
+        rospy.loginfo("No object. Finish the task")
+        return "finish"
+
+    def srv_home(self, req):
 
         print pose_res.count 
         rospy.loginfo("No object. Finish the task")
@@ -28,11 +40,11 @@ class arm_control(object):
 
 
 
-
 def main():
     rospy.init_node('arm_control')
-    rospy.wait_for_service(Detect_SRV)
-    rospy.wait_for_service(Pose_SRV)
+    rospy.wait_for_service(Joint_Server)
+    rospy.wait_for_service(Pose_Server)
+    rospy.wait_for_service(Straight_Server)
     rospy.spin()
 
 if __name__ == '__main__':
